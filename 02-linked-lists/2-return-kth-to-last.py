b4ebff14391eb158dc1835a0d7d45d7e8532e642
Lucas <lucasbivarfonseca@gmail.com>
2022-01-07 01:52:00 -0300

    adding some exercises from chapter 2

---
+++ 02-linked-lists/2-return-kth-to-last.py
@@ -0,0 +1,144 @@
+class LinkedListNode:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+def printLinkedList(linked_list):
+    linked_list_str = ""
+    while linked_list != None:
+        linked_list_str += str(linked_list.value) + "->"
+        linked_list=linked_list.next
+   
+    linked_list_str += "None"
+    print(linked_list_str)
+
+
+# Clarify questions
+# Can K be greater than linked list size? If yes, when we go to assign the 
+# p2 node reference we have to deal with it returning None, but in this case,
+# is not necessary, K will be always less or equal than the linked list size
+
+# Can the linked list be empty? Yes
+
+# Is the size of the list known? No
+
+# Should I return the node or the node value? In this case, I'm returning the node value
+
+# Approach #1 
+# Recursive 
+
+# Complexity:
+# Time: O(n)
+# Space: O(n)
+
+def printKthToLast(head, K):
+  if head == None:
+    return 0
+  
+  index = printKthToLast(head.next, K)+1
+
+  if index == K:
+    print(str(K)+"th to last node is "+str(head.value))
+
+  return index
+
+#Ex. 1: list 1->2->3->4->None, K = 1 | return 4
+LList = LinkedListNode(1)
+LList.next = LinkedListNode(2)
+LList.next.next = LinkedListNode(3)
+LList.next.next.next = LinkedListNode(4)
+
+printLinkedList(LList)
+printKthToLast(LList, 1)
+
+#Ex. 2: list 1->2->3->4->None, K = 2 | return 3
+LList = LinkedListNode(1)
+LList.next = LinkedListNode(2)
+LList.next.next = LinkedListNode(3)
+LList.next.next.next = LinkedListNode(4)
+
+printLinkedList(LList)
+printKthToLast(LList, 2)
+
+#Ex. 3: list 1->2->3->4->None, K = 3 | return 2
+LList = LinkedListNode(1)
+LList.next = LinkedListNode(2)
+LList.next.next = LinkedListNode(3)
+LList.next.next.next = LinkedListNode(4)
+
+printLinkedList(LList)
+printKthToLast(LList, 3)
+
+print("----------------")
+
+# Approach #2
+# two pointers
+# p1 = ^ and p2 = |
+
+# 1->2->3->4->5->6->none (k = 3)
+# ^     |
+# 1->2->3->4->5->6->none (k = 3)
+#    ^     |
+# 1->2->3->4->5->6->none (k = 3)
+#       ^     |
+# 1->2->3->4->5->6->none (k = 3)
+#          ^     | (p2.next = None -> end, so return the p1 value)
+
+# Complexity:
+# Time: O(n)
+# Space: O(1)
+
+def getKthToLast(head, K):
+  if head == None:
+    return None
+  
+  pointer1 = head
+  pointer2 = head
+  for i in range(1, K):
+    if pointer2.next == None: return None
+
+    pointer2 = pointer2.next
+  
+  while pointer2.next:
+    pointer1 = pointer1.next
+    pointer2 = pointer2.next
+  
+  return pointer1.value
+
+
+#Ex. 1: list 1->2->3->4->None, K = 1 | return 4
+LList = LinkedListNode(1)
+LList.next = LinkedListNode(2)
+LList.next.next = LinkedListNode(3)
+LList.next.next.next = LinkedListNode(4)
+
+printLinkedList(LList)
+print("Kth To Last:", getKthToLast(LList, 1))
+
+#Ex. 2: list 1->2->3->4->None, K = 2 | return 3
+LList = LinkedListNode(1)
+LList.next = LinkedListNode(2)
+LList.next.next = LinkedListNode(3)
+LList.next.next.next = LinkedListNode(4)
+
+printLinkedList(LList)
+print("Kth To Last:", getKthToLast(LList, 2))
+
+#Ex. 3: list 1->2->3->4->None, K = 3 | return 2
+LList = LinkedListNode(1)
+LList.next = LinkedListNode(2)
+LList.next.next = LinkedListNode(3)
+LList.next.next.next = LinkedListNode(4)
+
+printLinkedList(LList)
+print("Kth To Last:", getKthToLast(LList, 3))
+
+#Ex. 3: list 1->2->3->4->None, K = 5 | return None
+LList = LinkedListNode(1)
+LList.next = LinkedListNode(2)
+LList.next.next = LinkedListNode(3)
+LList.next.next.next = LinkedListNode(4)
+
+printLinkedList(LList)
+print("Kth To Last:", getKthToLast(LList, 5))
+
---
+++ 02-linked-lists/3-delete-middle-node.py
@@ -0,0 +1,68 @@
+class LinkedListNode:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+def printLinkedList(linked_list):
+    linked_list_str = ""
+    while linked_list != None:
+        linked_list_str += str(linked_list.value) + "->"
+        linked_list=linked_list.next
+   
+    linked_list_str += "None"
+    print(linked_list_str)
+
+# Clarify Questions
+# - Should I treat if the node is equal to None or the node is the last?
+# - Could I return a boolean value based on the operation's result?
+
+# Approach #1
+# If we have access the linked list we could iterate through the list
+# using previousNode and currentNode until currentNode reference be equal to
+# the node reference, so we just next to assing the node.next to prev.next
+
+# Complexity:
+# - Time: O(n) 
+# - Space: O(1)
+
+# Approach #2
+# 1. Assign the next node value to the current node
+# 2. Assign the node.next.next to node.next
+# 1 -> 2 -> 3 -> 4  (Node val = 2)
+
+# 1 -> 2 -> 3 -> 4  
+# 1 -> 3 -> 3 -> 4  
+# 1 -> 3 -> 4  
+
+# Complexity:
+# - Time: O(1) 
+# - Space: O(1)
+
+def  deleteMiddleNode(node: LinkedListNode) -> bool:
+  if node == None or node.next == None: return False
+
+  node.value = node.next.value
+  node.next = node.next.next
+
+  return True
+
+# Ex. 1: "a"->"b"->"c"->"d"->none, node "c" | new list: "a"->"b"->"d"->none
+LList = LinkedListNode("a")
+LList.next = LinkedListNode("b")
+LList.next.next = LinkedListNode("c")
+LList.next.next.next = LinkedListNode("d")
+
+printLinkedList(LList)
+print(deleteMiddleNode(LList.next.next))
+printLinkedList(LList)
+
+# Ex. 2: "a"->"b"->"c"->"d"->"e"->none, node "b" | new list: "a"->"c"->"d"->"e"->none
+LList = LinkedListNode("a")
+LList.next = LinkedListNode("b")
+LList.next.next = LinkedListNode("c")
+LList.next.next.next = LinkedListNode("d")
+LList.next.next.next.next = LinkedListNode("e")
+
+printLinkedList(LList)
+print(deleteMiddleNode(LList.next))
+printLinkedList(LList)
